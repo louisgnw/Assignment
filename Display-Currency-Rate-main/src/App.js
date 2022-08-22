1bd14e80470ff779909cf810ad2c66a4f746d94f
louisgnw <yosu998@gmail.com>
2022-08-22 19:08:16 +0700

    Assignment 2 Display Rate Currency

---
+++ Display-Currency-Rate-main/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import './App.css';
+import { Table } from "antd";
+import Moment from 'moment';
+
+
+import currencies from './data/data'
+
+
+function App() {
+  const [data, setData] = useState({});
+  const [date, setDate] = useState({});
+  const [loading, setloading] = useState(true);
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const { data } = await Axios.get("https://api.currencyfreaks.com/latest?apikey=d4035c0e9f0944589f29b1a9231e8056")
+      setloading(false);
+      console.log(data.rates)
+      console.log(data)
+      setData(data.rates)
+      setDate(data)
+    }
+    catch(error){
+      console.log(error);
+    }
+  };
+
+
+  const tableData =
+    currencies.map(({ currency, buy }) => ({
+    rate:  currency,
+    ExchangeRate: parseFloat(data[currency]),
+    WeBuy: parseFloat(data[currency]) + ((Math.floor(Math.random() * 4) + 1 ) / 100 *  parseFloat(data[currency])),
+    WeSell :  parseFloat(data[currency]) - ((Math.floor(Math.random() * 4) + 1 ) /100 *  parseFloat(data[currency]))
+    }))
+  
+  
+  const columns = [
+    {
+      title: "",
+      dataIndex: "rate",
+      width: 150
+    },
+    {
+      title: "We Buy",
+      dataIndex: "WeBuy",
+      width: 150
+    },
+    {
+      title: "Exchange Rate",
+      dataIndex: "ExchangeRate",
+      width: 150
+    },
+    {
+      title: "We Sell",
+      dataIndex: "WeSell",
+      width: 150
+    }
+  ];
+  
+
+  return (
+    <div className="App">
+      <h1>Display Rate Currency</h1>
+      <h1>Date : {Moment(date.date).format('YYYY-MM-DD')}</h1>
+      {loading ? (
+        "Loading"
+      ) : (
+        <Table
+          columns={columns}
+          dataSource={tableData}
+          style={{borderRadius:"10"}}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ Display-Currency-Rate-main/src/data/data.js
@@ -0,0 +1,23 @@
+const currencies = [
+    {
+        currency: "USD",
+    },
+    {
+        currency: "CAD",
+    },
+    {
+        currency: "IDR",
+    },
+    {
+        currency: "JPY",
+    },
+    {
+        currency: "CHF",
+    },
+    {
+        currency: "EUR",
+    }
+    
+];
+
+export default currencies;
